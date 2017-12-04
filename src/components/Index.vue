797cda8b4787e8326b4ef0472cfb7b6f44229ea9
wataru <woz.eon@gmail.com>
2017-12-04 16:58:47 +0900

    add store directory

---
+++ src/components/Index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import store from './store'
 import router from './router'
 
 Vue.config.productionTip = false
@@ -9,6 +10,7 @@ Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
   router,
   template: '<App/>',
   components: { App }
