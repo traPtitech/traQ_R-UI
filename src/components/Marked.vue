8ff98ac5aca122f33e3795843200dba4b2f13085
ninja <otakadai3111-1@yahoo.co.jp>
2017-12-05 13:32:25 +0900

    add markedRender

---
+++ src/components/Marked.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <p> {{ message }} </p>
+    <input v-model="input" v-on:keyup.enter="submitRender" />
+  </div>
+</template>
+
+<script>
+import marked from 'marked'
+
+export default {
+  name: 'Marked',
+  data () {
+    return {
+      input: '',
+      message: ''
+    }
+  },
+  methods: {
+    render: function (text) {
+      return marked(text)
+    },
+    submitRender: function () {
+      this.message = marked(this.input)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/components/Index'
+import Marked from '@/components/Marked'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
+      path: '/marked/',
+      name: 'Marked',
+      component: Marked
+    }, {
       path: '/',
       name: 'Index',
       component: Index
     }
-  ]
+  ],
+  mode: 'history'
 })
