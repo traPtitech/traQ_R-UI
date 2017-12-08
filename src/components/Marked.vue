a99d6e949d207a39e6e8d0c71f46c2c9c41fa545
ninja <otakadai3111-1@yahoo.co.jp>
2017-12-08 12:59:55 +0900

    ctrl+enterでポスト

--- src/components/Marked.vue
+++ src/components/Marked.vue
@@ -1,12 +1,31 @@
 <template>
-  <div>
-    <p> {{ message }} </p>
-    <input v-model="input" v-on:keyup.enter="submitRender" />
+  <div id="messages">
+    <p v-html="message"></p>
+    <textarea v-model="input" v-on:keyup.enter="submit($event.ctrlKey)" rows="5" />
   </div>
 </template>
 
 <script>
 import marked from 'marked'
+
+function escapeBefore (text) {
+  let replaced = ''
+  text.split(/```/g).forEach((e, i) => {
+    if (i % 2 === 0) replaced += e.replace(/</g, '&lt;')
+    replaced += e
+  })
+  return replaced
+}
+
+function escapeAfter (text) {
+  let rank = 0
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] === '<') rank++
+    else if (text[i] === '>' && rank > 0) rank--
+    else if (text[i] === '>') text = text.substr(0, i) + '&gt;' + text.substr(i + 1, text.length)
+  }
+  return text.replace(/&(?!gt;|lt;|quot;)/g, '&amp;').replace(/\n/g, '<br>')
+}
 
 export default {
   name: 'Marked',
@@ -17,16 +36,22 @@ export default {
     }
   },
   methods: {
-    render: function (text) {
-      return marked(text)
+    submit: function (ctrl) {
+      if (!ctrl) return
+      this.input.replace(/$\n/, '')
+      this.markdownRender()
     },
-    submitRender: function () {
-      this.message = marked(this.input)
+    markdownRender: function () {
+      this.message = escapeAfter(marked(escapeBefore(this.input)))
+      this.input = ''
     }
   }
 }
 </script>
 
 <style scoped>
-
+#messages {
+  text-align: left;
+  margin-top: 0px;
+}
 </style>
