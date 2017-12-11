bd466356f2080a9c55a57a8c5ef12c4079e85a98
ninja <otakadai3111-1@yahoo.co.jp>
2017-12-11 18:02:36 +0900

    add id, channel, stamp suggest

--- src/bin/completion.js
+++ src/bin/completion.js
@@ -1,16 +1,74 @@
 
 /*
  * @param {string} key - search key
- * @param {[string]} suggests - completion words
- * @param {number} num - completion limit
+ * @param {{string: [string]}} suggests - completion words
+ * @param {Object} option - suggest option
+ * option.limit = (3)
+ * option.ignoreCaptal = (false)
+ * option.multiKey = (false)
+ * : use array in suggests
  */
-export default function suggest (key, suggests, num) {
-  return suggests.filter((e) => {
-    if (num === 0) return false
-    if (e.substr(0, key.length) === key) {
-      num--
-      return true
+
+function toLower (str) {
+  if ('i'.toLowerCase() === 'i') {
+    return str.toLowerCase()
+  } else {
+    return str.replace(/[A-Z]/g, function (char) {
+      return String.fromCharCode(char.charCodeAt(0) | 32)
+    })
+  }
+}
+
+function isMatchWord (key, word, forceLower) {
+  if (forceLower) {
+    return toLower(key) === toLower(word.substr(0, key.length))
+  } else {
+    return key === word.substr(0, key.length)
+  }
+}
+
+function isMatchWords (key, words, forceLower) {
+  let ret = false
+  words.forEach(function (e) {
+    if (isMatchWord(key, e, forceLower)) {
+      ret = true
     }
-    return false
+  })
+  return ret
+}
+
+const LIMIT = 10
+const IGNORE_CAPITAL = false
+const MULTI_KEY = false
+
+export default function suggest (key, suggests, option) {
+  if (!option) {
+    option = {
+      limit: LIMIT,
+      ignoreCaptal: IGNORE_CAPITAL,
+      multiKey: MULTI_KEY
+    }
+  }
+  if (option.limit === undefined) {
+    option.limit = LIMIT
+  }
+  if (option.ignoreCaptal === undefined) {
+    option.ignoreCaptal = IGNORE_CAPITAL
+  }
+  if (option.multiKey === undefined) {
+    option.multiKey = MULTI_KEY
+  }
+  return Object.keys(suggests).filter(function (e) {
+    if (option.limit === 0) return false
+    if (option.multiKey) {
+      let ret = isMatchWords(key, suggests[e], option.ignoreCaptal)
+      ret |= isMatchWord(key, e, option.ignoreCaptal)
+      if (ret) option.limit--
+      return ret
+    } else {
+      let ret = isMatchWord(key, e, option.ignoreCaptal)
+      if (ret) option.limit--
+      return ret
+    }
   })
 }
--- src/components/Comp.vue
+++ src/components/Comp.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <textarea cols="50" rows="10" id="text" v-model="input"></textarea>
-    <p v-for="elem in candidates">
+    <p v-for="elem in searched.candidates">
       {{elem}}
     </p>
     <p> {{selectIndex}} </p>
@@ -24,10 +24,13 @@ export default {
     stealKeyEvent: function () {
       // disabling key event ↑, ↓, enter
       let self = this
+      document.getElementById('text').onclick = function () {
+        self.dummy ^= 1
+      }
       document.getElementById('text').onkeydown = function () {
         if (event.keyCode === 38 || event.keyCode === 40) {
           self.selectIndex += event.keyCode - 39
-          if (self.selectIndex >= self.candidates.length) self.selectIndex = self.candidates.length - 1
+          if (self.selectIndex >= self.searched.candidates.length) self.selectIndex = self.searched.candidates.length - 1
           if (self.selectIndex < 0) self.selectIndex = 0
           event.returnValue = false
         } else if (event.keyCode === 13) {
@@ -48,35 +51,86 @@ export default {
     unStealKeyEvent: function () {
       // release key event
       let self = this
-      // funvtion to calculate this.candidates
+      // function to calculate this.candidates
       document.getElementById('text').onkeydown = function () {
         self.dummy ^= 1
       }
     },
     replaceSuggest: function () {
       let selectionStart = document.getElementById('text').selectionStart
-      let strs = this.input.substr(0, selectionStart).split('#')
+      let strs = this.input.substr(0, selectionStart).split(this.searched.splitWord)
       let key = strs[strs.length - 1]
       let secondStr = this.input.substr(selectionStart, this.input.length)
 
       // replace key with candidates[index]
       let replaced = ''
       for (let i = 0; i < strs.length - 1; i++) {
-        replaced += strs[i] + '#'
+        replaced += strs[i] + this.searched.splitWord
       }
       let index = this.selectIndex
-      replaced += this.candidates[index] + ' ' + secondStr
-      this.input = replaced
+      replaced += this.searched.candidates[index] + this.searched.suffix + secondStr
 
       // change cursor position
-      let replacedSelectionStart = selectionStart + this.candidates[index].length - key.length + 1
+      let replacedSelectionStart = selectionStart + this.searched.candidates[index].length - key.length + this.searched.suffix.length
+      this.input = replaced
       setTimeout(function () {
         document.getElementById('text').selectionEnd = document.getElementById('text').selectionStart = replacedSelectionStart
       }, 10)
+    },
+    searchChannnels: function (str) {
+      let ret = {
+        splitWord: '#',
+        suffix: ' ',
+        candidates: [],
+        key: str
+      }
+      let splited = str.split('#')
+      if (splited.length < 2) return ret
+      let key = splited[splited.length - 1]
+      if (key.length === 0) return ret
+      ret.candidates = comp(key, words.channels, {
+        multiKey: true,
+        ignoreCaptal: true
+      })
+      return ret
+    },
+    searchIds: function (str) {
+      let ret = {
+        splitWord: '@',
+        suffix: ' ',
+        candidates: [],
+        key: str
+      }
+      let splited = str.split('@')
+      if (splited.length < 2) return ret
+      let key = splited[splited.length - 1]
+      // if (key.length === 0) return ret
+      ret.candidates = comp(key, words.ids, {
+        multiKey: false,
+        ignoreCaptal: true
+      })
+      return ret
+    },
+    searchStamps: function (str) {
+      let ret = {
+        splitWord: ':',
+        suffix: ':',
+        candidates: [],
+        key: str
+      }
+      let splited = str.split(':')
+      if (splited.length < 2) return ret
+      let key = splited[splited.length - 1]
+      if (key.length === 0) return ret
+      ret.candidates = comp(key, words.stamps, {
+        multiKey: false,
+        ignoreCaptal: true
+      })
+      return ret
     }
   },
   computed: {
-    candidates: function () {
+    searched: function () {
       if (this.input.length === 0) {
         return []
       }
@@ -95,49 +149,71 @@ export default {
       if (selectionStart !== selectionEnd) {
         return []
       }
-      let strs = this.input.substr(0, selectionStart).split('#')
-      // has no '#'
-      if (strs.length < 2) {
-        return []
-      }
-      let key = strs[strs.length - 1]
-      if (key.length === 0) {
-        return []
-      }
+
+      let str = this.input.substr(0, selectionStart)
 
       this.selectIndex = 0
-      let ret = comp(key, channels, 3)
-      if (ret.length > 0) this.stealKeyEvent()
+
+      let ret
+      ret = this.searchChannnels(str)
+      if (ret.candidates.length > 0) {
+        this.stealKeyEvent()
+        return ret
+      }
+      ret = this.searchIds(str)
+      if (ret.candidates.length > 0) {
+        this.stealKeyEvent()
+        return ret
+      }
+      ret = this.searchStamps(str)
+      if (ret.candidates.length > 0) {
+        this.stealKeyEvent()
+        return ret
+      }
       return ret
     }
   }
 }
 
-const channels = [
-  '17_2',
-  '17_2/ClayPlatesStory',
-  '17_2/Flythm',
-  '17_2/Inverse',
-  '17_2/NinjaFlicker',
-  '17_2/ShapeSphere2',
-  '17_2/SkyHighFight',
-  '17_2/TODOList',
-  '17_2/touhou',
-  '17_2/traQ',
-  'event',
-  'general',
-  'general/executive',
-  'general/meeting',
-  'general/schedule',
-  'gps',
-  'gps/buri',
-  'random',
-  'random/discussion',
-  'random/progress',
-  'team',
-  'traQ',
-  'univ'
-]
+const words = {
+  channels: {
+    '17_2': ['17_2'],
+    '17_2/ClayPlatesStory': ['ClayPlatesStory'],
+    '17_2/Flythm': ['Flythm'],
+    '17_2/Inverse': ['Inverse'],
+    '17_2/NinjaFlicker': ['NinjaFlicker'],
+    '17_2/ShapeSphere2': ['ShapeSphere2'],
+    '17_2/SkyHighFight': ['SkyHighFight'],
+    '17_2/TODOList': ['TODOList'],
+    '17_2/touhou': ['touhou'],
+    '17_2/traQ': ['traQ']
+  },
+  ids: {
+    'a': null,
+    'aa': null,
+    'ab': null,
+    'aaa': null,
+    'aab': null,
+    'aaaa': null,
+    'aaab': null,
+    'aba': null,
+    'abb': null,
+    'abba': null,
+    'abbb': null,
+    'b': null,
+    'ba': null,
+    'bb': null,
+    'bbb': null,
+    'bba': null,
+    'bbbb': null,
+    'bbbbb': null
+  },
+  stamps: {
+    'koko': null,
+    'po': null,
+    'yami': null
+  }
+}
 </script>
 
 <style scoped>
