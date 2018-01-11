1af68228569a0d97ccb805d9748ba3a8a961d9a7
ninja <otakadai3111-1@yahoo.co.jp>
2018-01-11 13:37:51 +0900

    auto textarea size fix

---
+++ src/components/Input.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <textarea v-model="text" :rows="row" >
+    </textarea>
+  </div>
+</template>
+
+<script>
+function countNL (s) {
+  return s.split('\n').length
+}
+
+export default {
+  name: 'Input',
+  data () {
+    return {
+      text: ''
+    }
+  },
+  computed: {
+    row: function () {
+      return Math.min(3, Math.max(1, countNL(this.text)))
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/components/Index'
+import Input from '@/components/Input'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Index',
       component: Index
+    }, {
+      path: '/input',
+      name: 'Input',
+      component: Input
     }
-  ]
+  ],
+  mode: 'history'
 })
