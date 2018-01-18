e11108661efdda98d4af4c5184ea1b7f2526de37
sigma <woz.eon@gmail.com>
2018-01-18 17:07:41 +0900

    add login page

--- src/components/Login/Login.vue
+++ src/components/Login/Login.vue
@@ -1,5 +1,12 @@
 <template lang="pug">
   div.login this is login page
+    form(method="POST" action="/login")
+      input(type="text" name="name" placeholder="ID または メールアドレス" required)
+      br
+      input(type="password" name="password" placeholder="パスワード" required)
+      br
+      button
+        | ログイン
     router-link(:to="{name: 'Index'}")
       p login
 </template>
