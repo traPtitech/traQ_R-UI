890c5871849e7e5b14f7d0453394dd9933421c47
ninja <otakadai3111-1@yahoo.co.jp>
2018-01-23 00:07:34 +0900

    add axios

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,19 +1,33 @@
 <template>
   <div>
-    <form method="POST" action="/login">
-      <input type="text" name="login" placeholder="IDまたはメールアドレス" required/>
+      <input v-model="name" type="text" name="login" placeholder="IDまたはメールアドレス" required/>
       <br>
-      <input type="password" name="password" placeholder="パスワード" required/>
-      <button>ログイン</button>
-    </form>
+      <input v-model="pass" type="password" name="password" placeholder="パスワード" required/>
+      <button v-on:click="loginPost()" >ログイン</button>
   </div>
 </template>
 
 <script>
+import Axios from 'axios'
+
 export default {
   name: 'Login',
   data () {
     return {
+      name: '',
+      pass: ''
+    }
+  },
+  methods: {
+    loginPost: function () {
+      Axios.post('/login', {
+        name: this.name,
+        pass: this.pass
+      }).then(function (res) {
+        console.log(res)
+      }).catch(function (err) {
+        console.log(err)
+      })
     }
   }
 }
