8f0860f105ee547bb5d118374bcf9ab4f753a126
n-inja <mccreepertakumi@gmail.com>
2018-03-03 16:09:51 +0900

    add json checker

--- src/bin/markdown-it.js
+++ src/bin/markdown-it.js
@@ -1,6 +1,8 @@
 import MarkdownIt from 'markdown-it'
 import hljs from 'highlight.js'
 import MarkdownItMark from 'markdown-it-mark'
+import iterator from 'markdown-it-for-inline'
+import store from '@/store/index'
 
 function highlight (code, lang) {
   const [langName, langCaption] = lang.split(':')
@@ -20,6 +22,61 @@ const md = new MarkdownIt({
 })
 
 md.use(MarkdownItMark)
+
+const jsonParse = (text) => {
+  console.log('json parse: ' + text)
+  try {
+    const data = JSON.parse(text)
+    if (typeof data['type'] !== 'string' || typeof data['raw'] !== 'string' || typeof data['id'] !== 'string') {
+      return '!' + text
+    }
+    if (data['type'] === 'user' && store.state.memberMap[data['id']]) {
+      return '[user reply]'
+    } else if (data['type'] === 'channel' && store.state.channelMap[data['id']]) {
+      return '[channel link]'
+    } else if (data['type'] === 'tag' && store.state.tagMap[data['id']]) {
+      return '[tag]'
+    } else if (data['type'] === 'file') {
+      return '[file]'
+    }
+    return text
+  } catch (e) {
+    return '!' + text
+  }
+}
+
+md.use(iterator, 'message-extends', 'text', (tokens, idx) => {
+  const text = tokens[idx].content
+  let replaced = ''
+  let isInside = false
+  let startIndex = -1
+  let isString = false
+  for (let i = 0; i < text.length; i++) {
+    if (isInside) {
+      if (text[i] === '\\') {
+        i++
+      } else if (text[i] === '"') {
+        isString ^= true
+      } else if (!isString && text[i] === '}') {
+        isInside = false
+        replaced += jsonParse(text.substr(startIndex + 1, i + 1 - startIndex))
+      }
+    } else {
+      if (i < text.length - 1 && text[i] === '!' && text[i + 1] === '{') {
+        startIndex = i
+        i++
+        isInside = true
+        isString = false
+      } else {
+        replaced += text[i]
+      }
+    }
+  }
+  if (isInside) {
+    replaced += text.substr(startIndex)
+  }
+  tokens[idx].content = replaced
+})
 md.disable('image')
 
 export default md
