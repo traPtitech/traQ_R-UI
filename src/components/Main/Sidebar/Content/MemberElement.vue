32215812304910ab5fc0fa3b6be216e1b1001724
n-inja <mccreepertakumi@gmail.com>
2018-03-11 01:23:26 +0900

    add user imformation

--- src/components/Main/Sidebar/Content/MemberElement.vue
+++ src/components/Main/Sidebar/Content/MemberElement.vue
@@ -1,6 +1,9 @@
 <template lang="pug">
 div
-  | {{model}}
+  div.list-user-icon-wrap
+    img.list-user-icon(:src="`${$store.state.baseURL}/api/1.0/files/${model.iconFileId}`")
+  p.list-user-name
+    | @{{model.name}}
 </template>
 
 <script>
@@ -13,4 +16,12 @@ export default {
 </script>
 
 <style lang="sass">
+.list-user-icon
+  width: 40px
+  height: 40px
+  background-color: gray
+  border-radius: 100%
+.list-user-name
+  margin: 0 0 0 10px
+  font-weight: bold
 </style>
