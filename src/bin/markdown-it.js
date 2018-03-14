d95324ff5b05c288f16df0bbff3116e0dd62d89a
n-inja <mccreepertakumi@gmail.com>
2018-03-14 10:36:50 +0900

    fix #115

--- src/bin/markdown-it.js
+++ src/bin/markdown-it.js
@@ -4,7 +4,7 @@ import MarkdownItMark from 'markdown-it-mark'
 import myPlugin from '@/bin/markdown-it-extends'
 import regexp from 'markdown-it-regexp'
 import store from '@/store/index'
-import milt from 'markdown-it-link-target'
+import mila from 'markdown-it-link-attributes'
 
 function highlight (code, lang) {
   const [langName, langCaption] = lang.split(':')
@@ -32,7 +32,12 @@ md.use(regexp(/:(\w+):/, (match, utils) => {
     return match[0]
   }
 }))
-md.use(milt)
+md.use(mila, {
+  attrs: {
+    target: '_blank',
+    rel: 'nofollow noopener noreferrer'
+  }
+})
 md.disable('image')
 
 export default md
