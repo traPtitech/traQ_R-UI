5d39f58ff58808d75405cb990f9c4a7750cb2f1c
n-inja <mccreepertakumi@gmail.com>
2018-03-25 19:44:26 +0900

    wip

--- src/components/Setting/Index.vue
+++ src/components/Setting/Index.vue
@@ -1,11 +1,29 @@
 <template lang="pug">
 div
- | setting
+  | setting
+  div
+    p
+      |  プロフィール設定
+  img.setting-user-icon(:src="`${$store.state.baseURL}/api/1.0/users/${$store.state.me.userId}/icon`")
 </template>
 
 <script>
-
+// import client from '@/bin/client'
+export default {
+  name: 'Setting',
+  data () {
+    return {
+    }
+  },
+  methods: {
+  }
+}
 </script>
 
-<style lang="scss">
+<style lang="sass">
+.setting-user-icon
+  width: 100px
+  height: 100px
+  background-color: white
+  border-radius: 100%
 </style>
