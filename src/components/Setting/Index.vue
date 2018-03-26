e235ffed5ddf338a1f9d4a29c2edc07fac4529a1
n-inja <mccreepertakumi@gmail.com>
2018-03-26 22:30:22 +0900

    add setting

--- src/components/Setting/Index.vue
+++ src/components/Setting/Index.vue
@@ -1,21 +1,81 @@
 <template lang="pug">
 div
-  | setting
+  h1
+    | setting
   div
-    p
+    h2
       |  プロフィール設定
-  img.setting-user-icon(:src="`${$store.state.baseURL}/api/1.0/users/${$store.state.me.userId}/icon`")
+    p
+      | 表示名
+      input(v-model="displayName")
+    img.setting-user-icon(:src="`${$store.state.baseURL}/api/1.0/users/${$store.state.me.userId}/icon`")
+    input(type="file" v-on:change="addFile")
+    button(v-on:click="submit")
+      | 更新
+    p(v-if="state === 'successed'")
+      | 変更されました
+    p(v-if="state === 'failed'")
+      | 失敗しました
+  div
+    h2
+      | スタンプ新規登録
+    input(type="file" v-on:change="addStampFile")
+    input(v-model="stampName")
+    button(v-if="stampFile && stampName.length > 0" v-on:click="addStamp")
+      | 追加
 </template>
 
 <script>
-// import client from '@/bin/client'
+import client from '@/bin/client'
 export default {
   name: 'Setting',
   data () {
     return {
+      displayName: '',
+      icon: null,
+      stampFile: null,
+      state: 'default',
+      stampName: ''
     }
   },
   methods: {
+    addFile (event) {
+      this.icon = event.target.files[0]
+    },
+    addStampFile (event) {
+      this.stampFile = event.target.files[0]
+    },
+    async submit () {
+      if (this.state === 'processing') {
+        return
+      }
+      this.state = 'processing'
+      const tasks = []
+      if (this.icon) {
+        tasks.push(client.changeIcon(this.icon))
+        this.icon = null
+      }
+      if (this.$store.state.me.displayName !== this.displayName) {
+        tasks.push(client.changeDisplayName(this.displayName))
+      }
+      Promise.all(tasks).then(() => {
+        this.state = 'successed'
+        this.$store.dispatch('whoAmI')
+      }).catch(e => {
+        this.state = 'failed'
+      })
+    },
+    addStamp () {
+      client.addStamp(this.stampName, this.stampFile)
+      .then(() => {
+        this.$store.dispatch('updateStamps')
+        this.stampFile = null
+        this.stampName = ''
+      })
+    }
+  },
+  mounted () {
+    this.displayName = this.$store.state.me.displayName
   }
 }
 </script>
