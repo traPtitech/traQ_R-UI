809621d47936345fa62c35b90ec215285b4c81ea
n-inja <mccreepertakumi@gmail.com>
2018-03-29 01:10:40 +0900

    add user element

--- src/components/Main/Sidebar/Content/MemberElement.vue
+++ src/components/Main/Sidebar/Content/MemberElement.vue
@@ -1,6 +1,11 @@
 <template lang="pug">
-div
-  | {{model}}
+div.member-element
+  p
+    img.member-element-icon(:src="`${$store.state.baseURL}/api/1.0/users/${model.userId}/icon`")
+    ruby
+      | {{model.displayName}}
+      rt
+        | @{{model.name}}
 </template>
 
 <script>
@@ -13,4 +18,15 @@ export default {
 </script>
 
 <style lang="sass">
+.member-element-icon
+  width: 40px
+  height: 40px
+  background-color: gray
+  border-radius: 100%
+.member-element > p
+  padding-left: 10%
+.member-element > p > ruby
+  padding-left: 5%
+  display: inline-block
+  vertical-align: 40%
 </style>
--- src/components/Main/Sidebar/Content/MemberList.vue
+++ src/components/Main/Sidebar/Content/MemberList.vue
@@ -1,17 +1,75 @@
 <template lang="pug">
 div
-  MemberElement(v-for="member in $store.state.memberData" :model="member" :key="member.userId")
+  div.grade-container(v-for="tag in tags" :class="`${gradeMap[tag.tag[2]]} ${tag.tag.substr(0, 2)}`" :style="`border-color: ${color(tag.tag)};`")
+    div.grade-name-container(:style="`background-color: ${color(tag.tag)};`")
+      p
+        | {{tag.tag}}
+    MemberElement(v-for="member in tag.users" :model="member" :key="member.userId")
 </template>
 
 <script>
 import MemberElement from '@/components/Main/Sidebar/Content/MemberElement'
 export default {
   name: 'MemberList',
+  data () {
+    return {
+      gradeMap: {
+        B: 'bachelor',
+        M: 'master',
+        D: 'doctor'
+      }
+    }
+  },
   components: {
     'MemberElement': MemberElement
+  },
+  methods: {
+    color (grade) {
+      const num = parseInt(grade.substr(0, 2), 10)
+      return this.colors[num][grade[2]]
+    }
+  },
+  computed: {
+    tags () {
+      const grades = this.$store.state.tagData.filter(tag => tag.tag !== '' && tag.tag.replace(/\d\d[B|M|D]/, '') === '')
+      const map = {
+        B: 0,
+        M: 1,
+        D: 2
+      }
+      const f = (s) => {
+        return map[s[2]] * 100 + parseInt(s.substr(0, 2), 10)
+      }
+      grades.sort((lhs, rhs) => {
+        return f(lhs.tag) - f(rhs.tag)
+      })
+      return grades
+    },
+    colors () {
+      const ret = new Array(100)
+      for (let i = 0; i < 100; i++) {
+        ret[i] = {
+          B: `hsl(${(i * 100) % 360}, 50%, 50%)`,
+          M: `hsl(${(i * 100 + 33) % 360}, 50%, 50%)`,
+          D: `hsl(${(i * 100 + 66) % 360}, 50%, 50%)`
+        }
+      }
+      return ret
+    }
   }
 }
 </script>
 
 <style lang="sass">
+.grade-container
+  text-align: left
+  border-left: 8px solid
+.grade-container > p
+  padding: 5px 10px
+.grade-name-container
+  width: 80%
+  margin-left: 5%
+.grade-name-container > p
+  color: white
+  margin-left: 10px
 </style>
