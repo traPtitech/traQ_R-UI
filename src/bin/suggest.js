0b193229cb9668f1f781c0ad5ca2f8254e4c2f91
n-inja <mccreepertakumi@gmail.com>
2018-03-29 15:51:49 +0900

    #9

---
+++ src/bin/suggest.js
@@ -0,0 +1,46 @@
+import store from '@/store'
+
+const match = (word, key) => {
+  return word.substr(0, key.length).toLowerCase() === key
+}
+
+export default function (key, limit) {
+  if (key.type === '#') {
+    return store.state.channelData
+    .filter(channel => channel.name !== '' && match(store.getters.getChannelPathById(channel.channelId), key.keyword))
+    .slice(0, limit)
+    .map(channel => {
+      return {
+        replace: store.getters.getChannelPathById(channel.channelId),
+        start: '#',
+        suffix: ' ',
+        html: `#${store.getters.getChannelPathById(channel.channelId)} `
+      }
+    })
+  } else if (key.type === '@') {
+    return store.state.memberData
+    .filter(user => match(user.name, key.keyword))
+    .slice(0, limit)
+    .map(user => {
+      return {
+        replace: user.name,
+        start: '@',
+        suffix: ' ',
+        html: `@${user.name} `
+      }
+    })
+  } else if (key.type === ':') {
+    return store.state.stampData
+    .filter(stamp => match(stamp.name, key.keyword))
+    .slice(0, limit)
+    .map(stamp => {
+      return {
+        replace: stamp.name,
+        start: ':',
+        suffix: ': ',
+        html: `<i class="emoji s16" title=":${stamp.name}:" style="background-image: url(${store.state.baseURL}/api/1.0/files/${stamp.fileId});">:${stamp.name}:</i>:${stamp.name}: `
+      }
+    })
+  }
+  return []
+}
