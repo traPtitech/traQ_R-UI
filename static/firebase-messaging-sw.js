09c2a9a7a9eb8da1a5722d21b83f1bb71c5e960a
to-hutohu <tohu.soy@gmail.com>
2018-04-02 19:57:32 +0900

    fix tokens

--- static/firebase-messaging-sw.js
+++ static/firebase-messaging-sw.js
@@ -7,7 +7,7 @@ importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js')
 // Initialize the Firebase app in the service worker by passing in the
 // messagingSenderId.
 firebase.initializeApp({
-  messagingSenderId: '179667783849'
+  messagingSenderId: '993645413001'
 })
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
