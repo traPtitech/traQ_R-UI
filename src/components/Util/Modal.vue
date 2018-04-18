acf8ed40c37877b075d250b7525800e6c1c61f02
sigma <woz2sow@gmail.com>
2018-04-18 23:29:28 +0900

    rename to stampbutton

--- src/components/Util/Modal.vue
+++ src/components/Util/Modal.vue
@@ -19,21 +19,18 @@ export default {
 
 <style lang="sass">
 .modal
-  position: fixed;
-  z-index: 101;
-  width: 80%;
-  background-color: #fff;
-  color: #000;
-
+  position: fixed
+  z-index: 101
+  width: 80%
+  background-color: #fff
+  color: #000
 .modal-overlay
-  position: fixed;
-  z-index: 100;
-  width: 100%;
-  height: 120%;
-  top: 0;
-  left: 0;
+  position: fixed
+  z-index: 100
+  width: 100%
+  height: 120%
+  top: 0
+  left: 0
   background-color: rgba(0, 0, 0, 0.75)
-
-
 </style>
 
