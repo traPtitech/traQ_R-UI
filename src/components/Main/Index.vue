ffb8a410237aaa711708511c02fb4b4afbd273f4
Eleven-Lines <yoyamssk@gmail.com>
2018-04-24 10:44:03 +0900

    Limit to landscape orientation

--- src/components/Main/Index.vue
+++ src/components/Main/Index.vue
@@ -284,7 +284,7 @@ export default {
     grid-template-rows: 60px 1fr
     grid-template-columns: 1fr 40px
     grid-template-areas: "titlebar titlebar""content information"
-  @media only screen and (device-width : 375px) and (device-height : 812px) and (-webkit-device-pixel-ratio : 3)
+  @media only screen and (device-width : 375px) and (device-height : 812px) and (-webkit-device-pixel-ratio : 3) and (orientation: landscape)
     grid-template-columns: calc(260px + env(safe-area-inset-left) - 7px) 1fr calc(40px + env(safe-area-inset-right) - 7px)
 
 </style>
