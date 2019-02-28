16a95cb8e358cf2677da7c8838f1b05de3f765f6
Eleven-Lines <yoyamssk@gmail.com>
2019-02-28 11:45:49 +0900

    fix #522

--- src/components/Main/Sidebar/Content/ClipList.vue
+++ src/components/Main/Sidebar/Content/ClipList.vue
@@ -1,12 +1,32 @@
 <template lang="pug">
-div
+.clip-list
+  .clip-list-under-construction(title="あのねあのね！")
+    IconLandscapeUnderConstruction(:size="128")
+    .clip-list-under-construction-text
+      | この先まだ工事中なんです
 </template>
 
 <script>
+import IconLandscapeUnderConstruction from '@/components/Icon/IconLandscapeUnderConstruction'
 export default {
-  name: 'ClipList'
+  name: 'ClipList',
+  components: {
+    IconLandscapeUnderConstruction
+  }
 }
 </script>
 
 <style lang="sass">
+.clip-list-under-construction
+  display: flex
+  flex-direction: column
+  align-items: center
+  color: white
+  padding-top: 5rem
+  user-select: none
+  opacity: 0.5
+.clip-list-under-construction-text
+  padding: 0 1rem
+  margin-top: 1rem
+  text-align: center
 </style>
