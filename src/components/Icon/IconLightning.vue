cec03895bde7b94c7a318f19d20077f85c50eb19
Eleven-Lines <yoyamssk@gmail.com>
2019-02-28 14:31:14 +0900

    add icon to channel list tab switcher

---
+++ src/components/Icon/IconLightning.vue
@@ -0,0 +1,21 @@
+<template functional>
+  <svg :width="props.size" :height="props.size" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M4 12.9429L15.6364 1L12 9.17143L20 11.0571L7.63636 23L10.5455 14.2L4 12.9429Z" :fill="props.color"/>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'IconLightning',
+  props: {
+    size: {
+      type: Number,
+      default: 16
+    },
+    color: {
+      type: String,
+      default: 'white'
+    }
+  }
+}
+</script>
