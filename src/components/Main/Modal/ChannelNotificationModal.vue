59ea46a181280e55ca451b8192dde6137b49b28e
Eleven-Lines <yoyamssk@gmail.com>
2019-04-26 01:08:42 +0900

    impl UI

--- src/components/Main/Modal/ChannelNotificationModal.vue
+++ src/components/Main/Modal/ChannelNotificationModal.vue
@@ -2,6 +2,13 @@
 base-common-modal.channel-notification-modal(title="NOTIFICATIONS" small)
   icon-notification-fill(color="var(--primary-color-on-bg)" slot="header-icon" size="24")
   p 通知を受け取るユーザーを選択
+  filter-input.notification-modal-filter(@input="searchQuery = $event.target.value" is-on-bg)
+  .notification-toggle-group-filtering-notice(v-if="isSearchQueryGroup && filterByGroup")
+    .notification-toggle-group-icon
+      icon-profile-fill(color="var(--primary-color-on-bg)" size="20")
+    .notification-toggle-group-info
+      p グループ「{{ searchQuery }}」で絞り込んでいます
+      a.notification-modal-link ID検索に戻す
   .notifications-item
     .notifications-members
       member-choice(v-for="notificationItem in membersWithNotificationStatus"
@@ -10,9 +17,9 @@ base-common-modal.channel-notification-modal(title="NOTIFICATIONS" small)
                    :key="notificationItem.member.userId"
                    v-model="notificationItem.status")
   .notifications-modal-footer
-    button(@click="$store.emit('closeModal')")
+    .notification-modal-button.notification-modal-button-passive(@click="$store.emit('closeModal')")
       | キャンセル
-    button(@click="submit")
+    .notification-modal-button.notification-modal-button-active(@click="submit")
       | 確定
 </template>
 
@@ -20,28 +27,37 @@ base-common-modal.channel-notification-modal(title="NOTIFICATIONS" small)
 import { mapState } from 'vuex'
 import MessageElement from '@/components/Main/MessageView/MessageElement/MessageElement'
 import MemberChoice from '@/components/Main/Modal/Util/MemberChoice'
+import FilterInput from '@/components/Util/FilterInput'
 import BaseCommonModal from '@/components/Main/Modal/BaseCommonModal'
 import IconNotificationFill from '@/components/Icon/IconNotificationFill'
+import IconProfileFill from '@/components/Icon/IconProfileFill'
 
 export default {
   name: 'ChannelNotificationModal',
   components: {
     MessageElement,
     MemberChoice,
+    FilterInput,
     BaseCommonModal,
-    IconNotificationFill
+    IconNotificationFill,
+    IconProfileFill
   },
   data() {
     return {
-      channelName: '',
-      state: 'default',
+      searchQuery: '',
       membersWithNotificationStatus: [],
       initialEnabledMembers: [],
-      initialDisabledMembers: []
+      initialDisabledMembers: [],
+      filterByGroup: true
     }
   },
   computed: {
-    ...mapState('modal', ['data'])
+    ...mapState('modal', ['data']),
+    isSearchQueryGroup() {
+      return new Set(this.$store.state.groupData.map(g => g.name)).has(
+        this.searchQuery
+      )
+    }
   },
   methods: {
     toggleMemberOff(userId) {
@@ -97,9 +113,29 @@ export default {
 </script>
 
 <style lang="sass">
+.channel-notification-modal
+  padding: 1rem 2rem
+
 .channel-notification-filter-wrap
   width: 13rem
   margin-left: auto
+
+.notification-modal-filter
+  margin: 1rem 0
+  input.filter-input
+    width: 100%
+
+.notification-toggle-group-filtering-notice
+  display: flex
+  align-items: center
+
+.notification-toggle-group-icon
+  margin-right: 1rem
+
+.notification-modal-link
+  margin-top: 0.25rem
+  color: $link-color
+  cursor: pointer
 
 .notifications-item
   margin: 1rem 0
@@ -132,5 +168,32 @@ export default {
 .notifications-modal-footer
   display: flex
   justify-content: flex-end
-  margin: 0 1rem
+  margin: 0 0.5rem
+
+.notification-modal-button
+  display: flex
+  justify-content: center
+  align-items: center
+  fot-size: 0.9rem
+  width: 6rem
+  height: 2.5rem
+  user-select: none
+  cursor: pointer
+  border:
+    radius: 4px
+    width: 1px
+    style: solid
+
+  +mq(sp)
+    flex-grow: 1
+
+  &:not(:first-child)
+    margin-left: 2rem
+
+  &-active
+    color: $primary-color-on-bg
+    border-color: $primary-color-on-bg
+  &-passive
+    color: gray
+    border-color: gray
 </style>
