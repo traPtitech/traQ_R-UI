3721f9521dd04fc6bb7101398cd859c0e42e3105
とーふとふ <tohu.soy@gmail.com>
2019-05-28 15:47:00 +0900

    型定義が反映されるように変更

--- src/types/shims-vue.d.ts
+++ src/types/shims-vue.d.ts
@@ -1,11 +1,3 @@
-import { ExStore } from 'vuex'
-
-declare module 'vue/types/vue' {
-  interface Vue {
-    $store: ExStore
-  }
-}
-
 declare module '*.vue' {
   import Vue from 'vue'
   export default Vue
