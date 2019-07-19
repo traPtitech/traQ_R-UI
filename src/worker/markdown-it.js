aca83fd268b6fa73272a00f3a16c4127476d84cb
sapphi-red <green@sapphi.red>
2019-07-19 17:43:37 +0900

    KaTeX

--- src/worker/markdown-it.js
+++ src/worker/markdown-it.js
@@ -4,6 +4,7 @@ import MarkdownItMark from 'markdown-it-mark'
 import spoiler from 'markdown-it-spoiler'
 import stamp, { renderStamp } from '@/worker/markdown-it-stamp'
 import json from '@/worker/markdown-it-json'
+import katex from '@iktakahiro/markdown-it-katex'
 import mila from 'markdown-it-link-attributes'
 import filter from 'markdown-it-image-filter'
 import whitelist from '@/bin/domain_whitelist.json'
@@ -59,7 +60,7 @@ md.block.State.prototype.skipEmptyLines = function skipEmptyLines(from) {
 md.use(MarkdownItMark)
 md.use(spoiler, true)
 md.use(json)
-md.use(stamp)
+md.use(katex)
 md.use(mila, {
   attrs: {
     target: '_blank',
