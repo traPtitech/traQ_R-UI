970084cbea978f95892bf3f7baca6b5f25fc863e
sapphi-red <green@sapphi.red>
2019-08-13 19:48:22 +0900

    padStart

--- src/components/Main/Modal/UserModal/UserModalProfile/UserModalProfileOnlineIndicator.vue
+++ src/components/Main/Modal/UserModal/UserModalProfile/UserModalProfileOnlineIndicator.vue
@@ -10,20 +10,17 @@
 <script>
 import { mapState } from 'vuex'
 
-function paddingNumber(n, k) {
-  let ret = `${n}`
-  for (let i = 0; i < k; i++) ret = '0' + ret
-  return ret.slice(-k)
-}
-function dateParse(date) {
-  return `${date.getFullYear()}/${paddingNumber(
+const paddingNumber = (n, k) => n.toString().padStart(k, '0')
+
+const dateParse = date =>
+  `${date.getFullYear()}/${paddingNumber(
     date.getMonth() + 1,
     2
   )}/${paddingNumber(date.getDate(), 2)} ${paddingNumber(
     date.getHours(),
     2
   )}:${paddingNumber(date.getMinutes(), 2)}`
-}
+
 export default {
   props: {
     detailed: {
