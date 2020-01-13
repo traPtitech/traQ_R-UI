ee86f09c422de15278e75742f10159f3a34d98a8
sapphi-red <green@sapphi.red>
2020-01-13 20:33:09 +0900

    modernモードでもjsのトランスパイルにbrowserslistが反映されるように

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
-module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"]
-};
+const vueCliPreset = require('@vue/cli-plugin-babel/preset')
+
+module.exports = api => {
+  api.cache(() => process.env.VUE_CLI_MODERN_BUILD)
+
+  const isModern = process.env.VUE_CLI_MODERN_BUILD
+
+  if (isModern) {
+    process.env.VUE_CLI_MODERN_BUILD = false
+  }
+  const config = vueCliPreset(api)
+  if (isModern) {
+    process.env.VUE_CLI_MODERN_BUILD = true
+  }
+  return config
+}
