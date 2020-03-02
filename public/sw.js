67e5c81e90dd54bbba5290ed919ffb5cae622d9c
sapphi-red <green@sapphi.red>
2020-03-03 04:48:50 +0900

    update packages

--- public/sw.js
+++ public/sw.js
@@ -191,8 +191,8 @@ self.addEventListener('notificationclick', event => {
 // Give the service worker access to Firebase Messaging.
 // Note that you can only use Firebase Messaging here, other Firebase libraries
 // are not available in the service worker.
-importScripts('https://www.gstatic.com/firebasejs/7.6.2/firebase-app.js')
-importScripts('https://www.gstatic.com/firebasejs/7.6.2/firebase-messaging.js')
+importScripts('https://www.gstatic.com/firebasejs/7.9.3/firebase-app.js')
+importScripts('https://www.gstatic.com/firebasejs/7.9.3/firebase-messaging.js')
 
 // Initialize the Firebase app in the service worker by passing in the
 // messagingSenderId.
